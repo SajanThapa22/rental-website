8a5937bc53f7d2568d73e6334720ab48353efee1
SajanThapa22 <sajanthapa1888@gmail.com>
2024-05-22 16:42:15 +0545

    making cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+
+      <div id="details">
+        <div>
+          <div>88888/ month</div>
+          <div>luxury villa in.....</div>
+        </div>
+
+        <div>
+          <div>3 bedrooms</div>
+          <div>2 bathrooms</div>
+          <div>2500 square fit</div>
+        </div>
+
+        <div id="property seller">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CardsContainer = () => {
-  return <div>CardsContainer</div>;
+  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>;
 };
 
 export default CardsContainer;
