5fcc875350d306f3f4e8cb4534b70ec73b8f2c30
SajanThapa22 <sajanthapa1888@gmail.com>
2024-05-25 14:44:45 +0545

    think im done with articles section

--- src/components/ArticleCards.jsx
+++ src/components/ArticleCards.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 
-const ArticleCards = ({ date, title, context, img, height }) => {
+const ArticleCards = ({ date, title, context, img, styles }) => {
   return (
-    <div className="gap-g-16px lg:gap-g-22px 2xl:gap-g-32px">
-      <div className={`${height}`}>
-        <img className="h-full w-full" src={img} alt="" />
+    <div
+      className={`flex flex-col lg:flex-row gap-4 lg:gap-g-20px 2xl:gap-8 ${styles}`}
+    >
+      <div className={`overflow-hidden`}>
+        <img
+          className="rounded-lg w-full aspect-[2/1] lg:aspect-[3/2] object-cover"
+          src={img}
+          alt=""
+        />
       </div>
 
-      <div className="gap-3 lg:gap-5 2xl:gap-8">
-        <div className="gap-2 lg:gap-5 2xl:gap-8">
-          <p id="date" className="font-bold text-font-small text-clr-primary">
+      <div className=" flex flex-col gap-2  2xl:gap-4">
+        <div className="flex flex-col gap-2 2xl:gap-4">
+          <p
+            id="date"
+            className="font-bold text-font-small text-clr-primary capitalize"
+          >
             {date}
           </p>
           <p
             id="title"
-            className="font-bold text-font-regular xl:text-f-20px text-clr-very-dark"
+            className="font-bold text-font-regular 2xl:text-f-20px text-clr-very-dark"
           >
             {title}
           </p>
         </div>
         <p
           id="context"
-          className="text-font-small xl:text-font-regular text-grayer"
+          className="text-font-small 2xl:text-font-regular text-grayer"
         >
           {context}
         </p>
--- src/components/ArticlesContainer.jsx
+++ src/components/ArticlesContainer.jsx
@@ -1,22 +1,73 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
 import ArticleCards from "./ArticleCards";
-import jaffna from "../assets/jaffna.png";
+
+import mainArticle from "../assets/mainArticle.png";
+import article1 from "../assets/article1.png";
+import article2 from "../assets/article2.png";
+import article3 from "../assets/article3.png";
+import article4 from "../assets/article4.png";
+
+const articles = [
+  {
+    img: article1,
+    date: "May 7, 2023",
+    title: "Car Rental Hacks for a Smooth Journey",
+    context:
+      "Learn insider tips for getting the best deals and making the most of your rental car experience.",
+  },
+  {
+    img: article4,
+    date: "April 25, 2023",
+    title: "Managing Your Rental Items: A Comprehensive Guide",
+    context:
+      "Get valuable insights on how to effectively manage and maintain your rented items.",
+  },
+  {
+    img: article3,
+    date: "January 2, 2023",
+    title: "Navigating the Rental Market: Insights for New Renters",
+    context:
+      "Explore key considerations and useful tips for individuals new to the rental market.",
+  },
+  {
+    img: article2,
+    date: "April 7, 2023",
+    title: "Maximizing Returns: Strategies for Rental Property Owners ",
+    context:
+      "Learn strategies to optimize the return on investment for your rental property.",
+  },
+];
 
 const ArticlesContainer = () => {
   return (
-    <div className="mt-mt-50 md:px-16 lg:px-20 px-p-16px">
+    <div className="mt-mt-50 md:px-16 lg:px-20 px-p-16px gap-g-24px flex flex-col md:gap-g-32px lg:gap-g-40px">
       <SectionTitle title="important articles" />
-      <div id="article-container" className="grid grid-cols-2">
-        <div>
+      <div
+        id="article-container"
+        className="grid grid-cols-1 gap-5 2xl:gap-8 lg:grid-cols-[5fr,4fr]"
+      >
+        <div className="h-full">
           <ArticleCards
-            img={jaffna}
-            date="february"
+            styles="grid grid-cols-1"
+            img={mainArticle}
+            date="february, 2023"
             title="Top Tips for Finding the Perfect Rental"
             context="Discover expert advice on how to find the ideal rental property that meets your needs and budget."
           />
         </div>
-        <div></div>
+        <div className="flex flex-col gap-4 2xl:gap-6">
+          {articles.map((a) => (
+            <ArticleCards
+              key={a.date}
+              img={a.img}
+              date={a.date}
+              title={a.title}
+              context={a.context}
+              styles="grid grid-cols-1 sm:grid-cols-[3fr,4fr]"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
